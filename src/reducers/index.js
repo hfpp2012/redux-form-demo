3fec77249d65175b03a9877c941964cdaf8f21d9
hfpp2012 <hfpp2012@gmail.com>
2018-08-20 19:26:31 +0800

    redux-form demo

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import users from './users';
+import { reducer as formReducer } from 'redux-form'
 
 export default combineReducers({
-  users
+  users,
+  form: formReducer
 });
